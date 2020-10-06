cf7bcb24b082dcb6eafc9cb2635e7e1440f02af2
Vladimir Kriukov <zeoniss28@gmail.com>
2020-10-06 22:21:21 +0300

    не работает ссылка 5 задачи

--- task-6.js
+++ task-6.js
@@ -1,11 +1,8 @@
-const input = document.querySelector('#validation-input')
-input.addEventListener('blur', function() {
-    const inputDataLength = input.getAttribute('data-length')
-    console.log(inputDataLength)
-
+const input = document.querySelector(`#validation-input`)
+input.addEventListener(`blur`, function() {
+    const inputDataLength = Number(input.getAttribute(`data-length`))
     const inputValueLength = input.value.length
-    console.log(inputValueLength)
-    if (inputDataLength < inputValueLength) {
+    if (inputDataLength === inputValueLength) {
         input.classList.add('valid')
         input.classList.remove('invalid')
     } else {
@@ -13,44 +10,3 @@ input.addEventListener('blur', function() {
         input.classList.remove('valid')
     }
 })
-
-// input.addEventListener(`blur`, function() {
-//     const inputDataLength = Number(input.getAttribute(`data-length`))
-//     const inputValueLength = String(input.value.length)
-//     if (inputDataLength === inputValueLength) {
-//         input.classList.add('valid')
-//         input.classList.remove('invalid')
-//     } else {
-//         input.classList.add('invalid')
-//         input.classList.remove('valid')
-//     }
-// })
-
-// function onCheckInputValue(event) {
-//     if (
-//         event.currentTarget.getAttribute('data-length') >
-//         event.currentTarget.value.length
-//     ) {
-//         elemInput.classList.add('invalid')
-//         elemInput.classList.remove('valid')
-//     } else {
-//         elemInput.classList.add('valid')
-//         elemInput.classList.remove('invalid')
-//     }
-// }
-// const validInputEl = document.querySelector('#validation-input')
-
-// validInputEl.addEventListener('blur', onInputValueCheck)
-
-// function onInputValueCheck(event) {
-//     if (
-//         event.currentTarget.getAttribute('data-length') ===
-//         event.currentTarget.value.length
-//     ) {
-//         validInputEl.classList.add('invalid')
-//         validInputEl.classList.remove('valid')
-//     } else {
-//         validInputEl.classList.add('valid')
-//         validInputEl.classList.remove('invalid')
-//     }
-// }
