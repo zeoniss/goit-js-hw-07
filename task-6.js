f4d09325839b5f206204eee3a6eb772fa12b2ab8
Vladimir Kriukov <zeoniss28@gmail.com>
2020-10-10 14:59:47 +0300

    task-6 fix

--- task-6.js
+++ task-6.js
@@ -1,12 +1,17 @@
 const input = document.querySelector(`#validation-input`)
-input.addEventListener(`blur`, function() {
-    const inputDataLength = Number(input.getAttribute(`data-length`))
-    const inputValueLength = input.value.length
-    if (inputDataLength === inputValueLength) {
+
+input.addEventListener(`blur`, lengthString)
+const lengthChecks = Number(input.dataset.length)
+
+function lengthString(event) {
+    if (input.value.length === lengthChecks) {
         input.classList.add('valid')
         input.classList.remove('invalid')
+    } else if (input.value.length === 0) {
+        input.classList.add('invalid')
+        input.classList.remove('valid')
     } else {
         input.classList.add('invalid')
         input.classList.remove('valid')
     }
-})
+}
